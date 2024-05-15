566ac838ff60b289df9d0e8b11452f37f04429c4
Justin Beckham Cardona Yepes <justinbeckhamc@gmail.com>
2024-05-14 22:04:53 -0500

    se agrega ruta obtenerListaEtapa

--- routes/etapa.routes.js
+++ routes/etapa.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import {crearEtapaHandler} from "../handlers/etapaHandlers.js"
+import {crearEtapaHandler, obtenerEtapasHandler} from "../handlers/etapaHandlers.js"
 
 const etapaRouter = Router();
 etapaRouter.post("/", crearEtapaHandler);
+etapaRouter.get("/lista", obtenerEtapasHandler);
 export default etapaRouter;
 
 
