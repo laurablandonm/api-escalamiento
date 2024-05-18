3b3d28ae09adf52e41ade83b374ed36bf3dcd43e
Justin Beckham Cardona Yepes <justinbeckhamc@gmail.com>
2024-05-18 00:09:04 -0500

    tipo proyecto y Etapa controllers handlers and routes generados

--- routes/etapa.routes.js
+++ routes/etapa.routes.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import {crearEtapaHandler, obtenerEtapasHandler} from "../handlers/etapaHandlers.js"
+import {crearEtapaHandler, editarEtapaHandler, eliminarEtapaHandler, obtenerEtapasHandler} from "../handlers/etapaHandlers.js"
+import { eliminarEtapa } from "../controllers/etapaControllers.js";
 
 const etapaRouter = Router();
 etapaRouter.post("/", crearEtapaHandler);
 etapaRouter.get("/lista", obtenerEtapasHandler);
+etapaRouter.put("/:id",editarEtapaHandler);
+etapaRouter.delete("/:id", eliminarEtapaHandler)
 export default etapaRouter;
+
+
+
+
+
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import etapaRouter from "./etapa.routes.js";
+import tipoDeproyectoRouter from "./tipoDeProyecto.routes.js";
 
 const router = Router();
 router.use("/etapa", etapaRouter);
+router.use("/tipodeproyecto", tipoDeproyectoRouter);
 
 export default router;
